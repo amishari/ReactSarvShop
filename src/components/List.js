3d788278feb38a77a2e2b991f21014202af41515
amishari <81028814+amishari@users.noreply.github.com>
2023-07-02 08:26:53 +0330

    Update List.js
    reply of searching in case of nothing found added

--- src/components/List.js
+++ src/components/List.js
@@ -2,7 +2,7 @@ import ShoppingCard from "./Card/ShoppingCard/ShoppingCard";
 import classes from "../components/Card/Card.module.css";
 const List = (props) => {
   if (props.list.length === 0) {
-    return <h2> چیزی یافت نشد. </h2>;
+    return <h2> 	چیزی یافت نشد. &#128580; . لطفا قسمتی از نام یا کد محصول را بنویسید</h2>;
   }
   return (
     <div className={classes.cards}>
